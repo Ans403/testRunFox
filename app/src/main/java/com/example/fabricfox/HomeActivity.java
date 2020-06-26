cbc45d128be867603c3a2bf6d8a072ad5ae77e85
Ans403 <ansbinzahid4@gmail.com>
2020-06-26 17:09:34 +0500

    1

--- app/src/main/java/com/example/fabricfox/HomeActivity.java
+++ app/src/main/java/com/example/fabricfox/HomeActivity.java
@@ -73,8 +73,15 @@ public class HomeActivity extends AppCompatActivity implements OnNavigationItemS
             }
         });
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        //  ActionBarDrawerToggle toggle = new ActionBarDrawerToggle()
+        
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
+
+            //  ActionBarDrawerToggle toggle = new ActionBarDrawerToggle()
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -152,9 +159,35 @@ public class HomeActivity extends AppCompatActivity implements OnNavigationItemS
                 || super.onSupportNavigateUp();
     }
 
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
 
-        @SuppressWarnings("StatementWithEmptyBody")
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+//        if (id == R.id.action_settings)
+//        {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
         @Override
         public boolean onNavigationItemSelected(MenuItem item){
         int id = item.getItemId();
